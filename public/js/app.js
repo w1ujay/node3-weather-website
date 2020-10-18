6d726ff19d3ba1ce048d09da1c3565627fde2dbc
w1ujay <w1ujay@gmail.com>
2020-10-17 20:14:17 -0400

    setup app for prod deployment

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,9 @@
 console.log('ClientSide Something or other')
 
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
-
-// messageOne.textContent = messageOne
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -17,7 +13,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading....'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if (data.error) {
             messageOne.textContent = data.error
--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 //Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -101,5 +101,5 @@ app.get('/*', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log('Server is up on port ', port)
+    console.log('Server is up on port ', + port)
 })
